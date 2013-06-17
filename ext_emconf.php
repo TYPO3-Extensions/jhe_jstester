d520c7f514b8a5011a3a31ad078da7cb96f01676
jhernst <jhernst@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-17 09:01:43 +0000

    * added the typo3 version dependency for the ter clean up process
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jhe_jstester/trunk@76604 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "jhe_jstester".
-#
-# Auto generated 08-09-2010 12:16
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jhe_jstester".
+ *
+ * Auto generated 17-06-2013 10:58
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'JavaScript-Tester',
@@ -29,16 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.5.0-4.7.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"0e10";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"2397";s:17:"ext_localconf.php";s:4:"05bb";s:14:"ext_tables.php";s:4:"45fe";s:13:"locallang.xml";s:4:"a667";s:16:"locallang_db.xml";s:4:"5c74";s:19:"doc/wizard_form.dat";s:4:"7c19";s:20:"doc/wizard_form.html";s:4:"aaf8";s:14:"pi1/ce_wiz.gif";s:4:"cf99";s:32:"pi1/class.tx_jhejstester_pi1.php";s:4:"54b9";s:40:"pi1/class.tx_jhejstester_pi1_wizicon.php";s:4:"2805";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"ec76";s:11:"res/css.css";s:4:"7160";s:20:"static/constants.txt";s:4:"45b5";s:16:"static/setup.txt";s:4:"9968";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"ec2d";s:12:"ext_icon.gif";s:4:"2397";s:17:"ext_localconf.php";s:4:"05bb";s:14:"ext_tables.php";s:4:"45fe";s:13:"locallang.xml";s:4:"a667";s:16:"locallang_db.xml";s:4:"5c74";s:10:"README.txt";s:4:"9fa9";s:19:"doc/wizard_form.dat";s:4:"7c19";s:20:"doc/wizard_form.html";s:4:"aaf8";s:14:"pi1/ce_wiz.gif";s:4:"cf99";s:32:"pi1/class.tx_jhejstester_pi1.php";s:4:"54b9";s:40:"pi1/class.tx_jhejstester_pi1_wizicon.php";s:4:"2805";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"ec76";s:11:"res/css.css";s:4:"7160";s:20:"static/constants.txt";s:4:"45b5";s:16:"static/setup.txt";s:4:"9968";}',
 	'suggests' => array(
 	),
 );
